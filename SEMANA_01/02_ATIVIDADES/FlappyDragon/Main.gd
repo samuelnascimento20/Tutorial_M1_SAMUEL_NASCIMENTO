73d6e787c08d6f8de1c04b42141e72a50a8ac10f
samuelnascimento20 <124221367+samuelnascimento20@users.noreply.github.com>
2023-02-02 15:25:52 -0300

    Update Main.gd

--- SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
+++ SEMANA_01/02_ATIVIDADES/FlappyDragon/Main.gd
@@ -1,9 +1,13 @@
 extends Node # instancia a classe Node2D
 
-var status = 1
+var status = 1 
 var vscore = 0
-var x = 1.5 
-var y = 1.5 
+var x = 1.5
+# X é a constante que altera a posição no plano horizontal
+# Y é a constante que altera a posição no plano vertical
+# Status é uma variável que inicia o jogo quando é igual a 1 ou encerra o jogo quando é igual a 0
+# vscore é o placar 
+var y = 5.0
 
 # executa essa função ao carregar o jogo
 func _ready():
@@ -41,11 +45,11 @@ func _process(delta):
 			
 		# se apertou seta para baixo, aumenta o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_down"):
-			$dragon.position.y += 2
+			$dragon.position.y += 4
 
 		# se apertou seta para cima, diminui o valor de y (posição vertical) do dragão
 		if Input.is_action_pressed("ui_up"):
-			$dragon.position.y -= 4
+			$dragon.position.y -= 10
 			
 	elif status == 0: # parado
 		
