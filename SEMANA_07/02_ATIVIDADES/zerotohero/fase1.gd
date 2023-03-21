bf7c5d729090590f206c311cc7a46beafdba8e4f
samuelnascimento20 <124221367+samuelnascimento20@users.noreply.github.com>
2023-03-21 13:42:56 -0300

    código comentado

--- SEMANA_07/02_ATIVIDADES/zerotohero/fase1.gd
+++ SEMANA_07/02_ATIVIDADES/zerotohero/fase1.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
+#tocar música no jogo
 func _ready():
 	$AudioStreamPlayer2D.play()
-
+	
+#movimentação do perosnagem
 func _physics_process(delta):
 	var movimento = Vector2.ZERO
 
@@ -11,5 +13,6 @@ func _physics_process(delta):
 	movimento.y = Input.get_action_strength("ui_down")-Input.get_action_strength("ui_up")
 	move_and_slide(movimento*100)
 
+#trocar de cena ao chegar no final da fase
 func _on_Area2D_body_entered(body):
 	get_tree().change_scene("res://fase2.tscn")
--- SEMANA_07/02_ATIVIDADES/zerotohero/telainicial.gd
+++ SEMANA_07/02_ATIVIDADES/zerotohero/telainicial.gd
@@ -1,9 +1,9 @@
 extends Node2D
  
-
+#tocar música no menu
 func _ready():
 	$AudioStreamPlayer2D.play()
 
-
+#trocar de cena
 func _on_Button_pressed():
 	get_tree().change_scene("res://fase1.tscn")
